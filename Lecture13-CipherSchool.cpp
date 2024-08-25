53f5295f836ab47574d527cb6d6f687361bb1389
adityrajtiwary <122718170+adityrajtiwary@users.noreply.github.com>
2024-08-25 09:17:31 +0530

    Add files via upload

---
+++ Lecture13-CipherSchool.cpp
@@ -0,0 +1,12 @@
+//Here we are going to learn about function 
+//function is nothing but a piece of code where it will only going to get execute when we call them 
+//whenever we run any code the first thing which executed is the main function 
+
+#include<iostream>
+using namespace std;
+
+int main()  // This is the main function 
+{
+	//whenever we write anything here the compiler first comes and call the main fucntion and whatever code is residing inside this curly braces it starts to execute them 
+	return 0;
+}
